b0ac504f29f3c218d28585959918e62c34e3c21e
Rishav Krishna <rishavkrishna22@gmail.com>
2018-11-21 22:10:44 +0530

    re-captcha

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/',function(req,res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit',function(req,res){
+
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+  }
+ 
+  var secretKey = "6LefRXwUAAAAALbVUfZ5c9RovzunGv2G2P8-FpXJ";
+ 
+  var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+  request(verificationUrl,function(error,response,body) {
+    body = JSON.parse(body);
+ 
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
+  });
+});
+app.use("*",function(req,res) {
+  res.status(404).send("404");
+})
+
+app.listen(3000, () => {
+	console.log("app is listening on port 3000");	
+});
